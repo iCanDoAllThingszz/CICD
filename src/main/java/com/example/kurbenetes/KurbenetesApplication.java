7f4fff6572e5a4ab6f80f9bfeea327d8380b3d13
zhaoyu93 <zhaoyu93@meituan.com>
2025-01-20 22:32:10 +0800

    first commit

---
+++ src/main/java/com/example/kurbenetes/KurbenetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurbenetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurbenetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurbenetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kurbenetes/KurbenetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurbenetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurbenetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
